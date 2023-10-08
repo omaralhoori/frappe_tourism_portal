0a64402badcb2fafca271c063d2e009e3c5e03ed
Omar <omaralhoori@gmail.com>
2023-10-08 15:55:43 +0300

    create first doctypes

---
+++ tourism_portal/tourism_portal/doctype/room_accommodation_type/room_accommodation_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, omaralhoori and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Room Accommodation Type', {
+	// refresh: function(frm) {
+
+	// }
+});
