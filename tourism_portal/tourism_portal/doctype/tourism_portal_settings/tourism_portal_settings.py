3e5b6e55ec8269b59d85b1a6429e2f5d2789aac6
Omar <omaralhoori@gmail.com>
2023-10-09 11:44:46 +0300

    add more doctypes

---
+++ tourism_portal/tourism_portal/doctype/tourism_portal_settings/tourism_portal_settings.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2023, omaralhoori and contributors
+# For license information, please see license.txt
+
+import frappe
+import frappe.defaults
+
+from frappe.model.document import Document
+
+
+portal_keydict = {
+	# "key in defaults": "key in Global Defaults"
+	"hotel_cancellation_policy": "default_hotel_cancellation_policy",
+	"hotel_accommodation_type_rule": "default_hotel_accommodation_type_rule",
+	"hotel_profit_margin": "default_hotel_profit_margin",
+}
+
+
+class TourismPortalSettings(Document):
+	def on_update(self):
+		"""update defaults"""
+		for key in portal_keydict:
+			frappe.db.set_default(key, self.get(portal_keydict[key], ""))
+
+		# clear cache
+		frappe.clear_cache()
+
+	def get_defaults(self):
+		return frappe.defaults.get_defaults()
