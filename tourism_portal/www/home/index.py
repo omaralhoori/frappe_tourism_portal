5c89bc089baebff8fe5b53926be1924995be85d6
Omar <omaralhoori@gmail.com>
2024-01-13 10:42:32 +0000

    no_cache home

--- tourism_portal/www/home/index.py
+++ tourism_portal/www/home/index.py
@@ -5,7 +5,9 @@ import frappe
 from frappe import _
 from tourism_portal.utils import get_portal_setting
 
+no_cache=1
 def get_context(context):
+	context.no_cache=1
 	if frappe.session.user == "Guest":
 		frappe.throw(_("Log in to access this page."), frappe.PermissionError)
 	
