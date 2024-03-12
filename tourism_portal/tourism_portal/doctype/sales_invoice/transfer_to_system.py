3eeec7f56b5223aeaa5d7130a3addb0bd9e0c7e2
Omar <omaralhoori@gmail.com>
2024-03-12 12:19:52 +0000

    edit top bar, add sys code

--- tourism_portal/tourism_portal/doctype/sales_invoice/transfer_to_system.py
+++ tourism_portal/tourism_portal/doctype/sales_invoice/transfer_to_system.py
@@ -1,26 +1,40 @@
 import frappe
 
 @frappe.whitelist()
-def get_invoice_data(invoice):
+def get_invoice_data_2(invoice):
     if type(invoice) == str:
         invoice = frappe.get_doc("Sales Invoice", invoice)
     return invoice.as_dict()
+
+@frappe.whitelist()
+def get_invoice_data(invoice):
+    invoice = frappe.get_doc("Sales Invoice", invoice)
     invoice_details = {
-        "customer": invoice.customer,
-        "posting_date": invoice.posting_date,
-        "due_date": invoice.due_date,
-        "grand_total": invoice.grand_total,
-        "outstanding_amount": invoice.outstanding_amount,
-        "currency": invoice.currency,
-        "rooms": invoice.rooms,
-        "transfers": invoice.transfers,
-        "tours": invoice.tours,
-        "other_services": invoice.other_services,
-        "total_rooms": len(invoice.rooms),
-        "total_transfers": len(invoice.transfers),
-        "total_tours": len(invoice.tours),
-        "total_other_services": len(invoice.other_services),
-        "total_services": len(invoice.rooms) + len(invoice.transfers) + len(invoice.tours) + len(invoice.other_services),
-        "total_services_amount": sum([i.amount for i in invoice.rooms] + [i.amount for i in invoice.transfers] + [i.amount for i in invoice.tours] + [i.amount for i in invoice.other_services])
+        **get_invoice_details(invoice),
     }
     return invoice_details
+
+def get_invoice_details(invoice):
+    invoice_details = {
+        "id": invoice.name, 
+        "turop": frappe.db.get_value("Company",invoice.company, "system_code", cache=True), # Turop - Get from company
+        "invoice_no": invoice.invoice_no, 
+        "voucher": invoice.voucher_no.split("-")[-1], # Voucher - Split and get last number
+        "post_date": invoice.post_date, # AcTarihi, UpdTarihi
+        "post_time": invoice.post_time,
+        "invoice_check_out": invoice.invoice_check_out,
+        "status": invoice.status,
+        "transfer_total": invoice.transfer_fees_company,
+        "tour_total": invoice.tour_fees_company,
+        "hotel_total": invoice.hotel_fees_company,
+        "grand_total": invoice.grand_total_company,
+    }
+    return invoice_details
+
+def get_customer_details(invoice):
+    customers = {} 
+
+# def get_hotel_details(invoice):
+#     hotels = {}
+#     main_count = 0
+#     for 
