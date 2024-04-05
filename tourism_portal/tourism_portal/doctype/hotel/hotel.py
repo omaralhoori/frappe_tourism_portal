e9d3815dfb45bd029aea9aa8e1821ed2328efd58
Omar <omaralhoori@gmail.com>
2024-04-05 13:30:58 +0000

    add hotel Priority

--- tourism_portal/tourism_portal/doctype/hotel/hotel.py
+++ tourism_portal/tourism_portal/doctype/hotel/hotel.py
@@ -5,4 +5,12 @@
 from frappe.model.document import Document
 
 class Hotel(Document):
-	pass
+	def before_save(self):
+		self.set_priority()
+	def set_priority(self):
+		if self.priority == 'High Priority':
+			self.hotel_priority = 10
+		elif self.priority == 'Medium Priority':
+			self.hotel_priority = 5
+		elif self.priority == 'Low Priority':
+			self.hotel_priority = 2
